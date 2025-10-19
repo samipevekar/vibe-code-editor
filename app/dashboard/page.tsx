e0017797835a5803b8eedc0ce5af860210d801bf
samipevekar <samipevekar499@gmail.com>
2025-10-19 11:56:56 +0530

    05: Dashboard Part 2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,33 +1,41 @@
-import EmptyState from '@/components/ui/empty-state'
-import AddNewButton from '@/features/dashboard/components/add-new-button'
-import AddRepoButton from '@/features/dashboard/components/add-repo-button'
-import React from 'react'
+import EmptyState from "@/components/ui/empty-state";
+import { deleteProjectById, duplicateProjectById, editProjectById, getAllPlaygroundForUser } from "@/features/dashboard/actions";
+import AddNewButton from "@/features/dashboard/components/add-new-button";
+import AddRepoButton from "@/features/dashboard/components/add-repo-button";
+import ProjectTable from "@/features/dashboard/components/project-table";
+import React from "react";
 
-const page = () => {
-
-  const playgrounds:any[] = []
+const page = async () => {
+  const playgrounds = await getAllPlaygroundForUser();
 
   return (
     <div>
-      <div className='flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 w-full'>
-          <AddNewButton/>
-          <AddRepoButton/>
+      <div className="flex flex-col justify-start items-center min-h-screen mx-auto max-w-7xl px-4 py-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
+          <AddNewButton />
+          <AddRepoButton />
         </div>
 
-        <div className='mt-10 flex flex-col justify-center items-center w-full'>
-          {
-            playgrounds && playgrounds.length === 0 ? (<EmptyState title='No Projects Found' description='Create a new project to get started' imageSrc='/empty-state.svg' />) : (
-              // todo add laygoru table
-              <p>
-                List of Playgrounds
-              </p>
-            )
-          }
+        <div className="mt-10 flex flex-col justify-center items-center w-full">
+          {playgrounds && playgrounds.length === 0 ? (
+            <EmptyState
+              title="No Projects Found"
+              description="Create a new project to get started"
+              imageSrc="/empty-state.svg"
+            />
+          ) : (
+            // todo add laygoru table
+            <ProjectTable
+              projects={playgrounds || []}
+              onDeleteProject={deleteProjectById}
+              onUpdateProject={editProjectById}
+              onDuplicateProject={duplicateProjectById}
+            />
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
