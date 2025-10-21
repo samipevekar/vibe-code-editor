d1822ed97e4780803adc2e6e0eb6c1485c8f2651
samipevekar <samipevekar499@gmail.com>
2025-10-21 12:45:05 +0530

    08: Playground Editor File Explorer Part 1

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+    REACT: '/vibecode-starters/react-ts',
+    NEXT: '/vibecode-starters/nextjs-new',
+    VUE: '/vibecode-starters/vue',
+    ANGULAR: '/vibecode-starters/angular',
+    EXPRESS: '/vibecode-starters/express-simple',
+    HONO: '/vibecode-starters/hono-nodejs-starter',
+}
